be3f280ae4f7988c83a4e5e5e6e4de9a3fee5a00
NekoGirlSAIKOU <nekogirlsaikou@outlook.com>
2022-02-07 20:26:26 +0800

    Fix QueryIntentActivitiesHook

--- app/src/main/java/com/tsng/hidemyapplist/xposed/hooks/method/QueryIntentActivitiesHook.kt
+++ app/src/main/java/com/tsng/hidemyapplist/xposed/hooks/method/QueryIntentActivitiesHook.kt
@@ -2,16 +2,21 @@ package com.tsng.hidemyapplist.xposed.hooks.method
 
 import android.content.pm.PackageManager
 import android.content.pm.ResolveInfo
+import android.util.Log
 import com.tsng.hidemyapplist.xposed.hooks.EnhancedIndividualHooks
 import de.robv.android.xposed.XC_MethodHook
 
 class QueryIntentActivitiesHook:XC_MethodHook() {
+    companion object{
+        private const val TAG = "QueryIntentActivitiesHook"
+    }
+
     override fun afterHookedMethod(param: MethodHookParam?) {
         super.afterHookedMethod(param)
         if (param != null){
             val result = param.result as MutableList<ResolveInfo>
             param.result = result.filter {
-                !EnhancedIndividualHooks.isToHidePackage(it.resolvePackageName)
+                !EnhancedIndividualHooks.isToHidePackage(it.activityInfo.packageName)
             }.toMutableList()
         }
     }
